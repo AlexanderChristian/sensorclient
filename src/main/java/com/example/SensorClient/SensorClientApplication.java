57c5390ecec1dd479aafe1fcc70f12bcd8ad01bf
AlexanderChristian <acignat@gmail.com>
2024-10-06 13:04:57 +0300

    Initial commit

---
+++ src/main/java/com/example/SensorClient/SensorClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.SensorClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SensorClient/SensorClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SensorClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
